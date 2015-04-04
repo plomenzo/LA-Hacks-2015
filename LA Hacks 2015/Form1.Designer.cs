61f1e316779a7bea9c1f0ea22050f4b79a5f6eb2
Jose Lascano <lascano.jose@yahoo.com>
2015-04-04 05:18:39 -0700

    Added Drag and Drop
    On t2 for now

--- LA Hacks 2015/Form1.Designer.cs
+++ LA Hacks 2015/Form1.Designer.cs
@@ -34,10 +34,8 @@
             this.Start = new System.Windows.Forms.Button();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.t1 = new System.Windows.Forms.TabPage();
-            this.t2 = new System.Windows.Forms.TabPage();
             this.t3 = new System.Windows.Forms.TabPage();
             this.TEST = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.t4 = new System.Windows.Forms.TabPage();
             this.t14 = new System.Windows.Forms.TabPage();
             this.t15 = new System.Windows.Forms.TabPage();
@@ -56,10 +54,19 @@
             this.t11 = new System.Windows.Forms.TabPage();
             this.t12 = new System.Windows.Forms.TabPage();
             this.t13 = new System.Windows.Forms.TabPage();
+            this.sol1 = new System.Windows.Forms.PictureBox();
+            this.t2 = new System.Windows.Forms.TabPage();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.tabControl1.SuspendLayout();
             this.t1.SuspendLayout();
-            this.t2.SuspendLayout();
             this.t3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.sol1)).BeginInit();
+            this.t2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
             // Start
@@ -115,24 +122,13 @@
             this.t1.Text = "tabPage1";
             this.t1.UseVisualStyleBackColor = true;
             // 
-            // t2
-            // 
-            this.t2.Controls.Add(this.textBox1);
-            this.t2.Location = new System.Drawing.Point(4, 22);
-            this.t2.Name = "t2";
-            this.t2.Padding = new System.Windows.Forms.Padding(3);
-            this.t2.Size = new System.Drawing.Size(750, 390);
-            this.t2.TabIndex = 1;
-            this.t2.Text = "t2";
-            this.t2.UseVisualStyleBackColor = true;
-            // 
             // t3
             // 
             this.t3.Controls.Add(this.TEST);
             this.t3.Location = new System.Drawing.Point(4, 22);
             this.t3.Name = "t3";
             this.t3.Padding = new System.Windows.Forms.Padding(3);
-            this.t3.Size = new System.Drawing.Size(750, 390);
+            this.t3.Size = new System.Drawing.Size(753, 390);
             this.t3.TabIndex = 2;
             this.t3.Text = "tabPage3";
             this.t3.UseVisualStyleBackColor = true;
@@ -146,21 +142,12 @@
             this.TEST.Text = "TEST";
             this.TEST.UseVisualStyleBackColor = true;
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(367, 147);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "THIS IS TAB 2";
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
             // t4
             // 
             this.t4.Location = new System.Drawing.Point(4, 22);
             this.t4.Name = "t4";
             this.t4.Padding = new System.Windows.Forms.Padding(3);
-            this.t4.Size = new System.Drawing.Size(750, 390);
+            this.t4.Size = new System.Drawing.Size(753, 390);
             this.t4.TabIndex = 3;
             this.t4.Text = "tabPage1";
             this.t4.UseVisualStyleBackColor = true;
@@ -170,7 +157,7 @@
             this.t14.Location = new System.Drawing.Point(4, 22);
             this.t14.Name = "t14";
             this.t14.Padding = new System.Windows.Forms.Padding(3);
-            this.t14.Size = new System.Drawing.Size(750, 390);
+            this.t14.Size = new System.Drawing.Size(753, 390);
             this.t14.TabIndex = 4;
             this.t14.Text = "tabPage2";
             this.t14.UseVisualStyleBackColor = true;
@@ -180,7 +167,7 @@
             this.t15.Location = new System.Drawing.Point(4, 22);
             this.t15.Name = "t15";
             this.t15.Padding = new System.Windows.Forms.Padding(3);
-            this.t15.Size = new System.Drawing.Size(750, 390);
+            this.t15.Size = new System.Drawing.Size(753, 390);
             this.t15.TabIndex = 5;
             this.t15.Text = "tabPage3";
             this.t15.UseVisualStyleBackColor = true;
@@ -190,7 +177,7 @@
             this.t16.Location = new System.Drawing.Point(4, 22);
             this.t16.Name = "t16";
             this.t16.Padding = new System.Windows.Forms.Padding(3);
-            this.t16.Size = new System.Drawing.Size(750, 390);
+            this.t16.Size = new System.Drawing.Size(753, 390);
             this.t16.TabIndex = 6;
             this.t16.Text = "tabPage4";
             this.t16.UseVisualStyleBackColor = true;
@@ -200,7 +187,7 @@
             this.t17.Location = new System.Drawing.Point(4, 22);
             this.t17.Name = "t17";
             this.t17.Padding = new System.Windows.Forms.Padding(3);
-            this.t17.Size = new System.Drawing.Size(750, 390);
+            this.t17.Size = new System.Drawing.Size(753, 390);
             this.t17.TabIndex = 7;
             this.t17.Text = "tabPage5";
             this.t17.UseVisualStyleBackColor = true;
@@ -210,7 +197,7 @@
             this.t18.Location = new System.Drawing.Point(4, 22);
             this.t18.Name = "t18";
             this.t18.Padding = new System.Windows.Forms.Padding(3);
-            this.t18.Size = new System.Drawing.Size(750, 390);
+            this.t18.Size = new System.Drawing.Size(753, 390);
             this.t18.TabIndex = 8;
             this.t18.Text = "tabPage6";
             this.t18.UseVisualStyleBackColor = true;
@@ -220,7 +207,7 @@
             this.t19.Location = new System.Drawing.Point(4, 22);
             this.t19.Name = "t19";
             this.t19.Padding = new System.Windows.Forms.Padding(3);
-            this.t19.Size = new System.Drawing.Size(750, 390);
+            this.t19.Size = new System.Drawing.Size(753, 390);
             this.t19.TabIndex = 9;
             this.t19.Text = "tabPage7";
             this.t19.UseVisualStyleBackColor = true;
@@ -230,7 +217,7 @@
             this.t20.Location = new System.Drawing.Point(4, 22);
             this.t20.Name = "t20";
             this.t20.Padding = new System.Windows.Forms.Padding(3);
-            this.t20.Size = new System.Drawing.Size(750, 390);
+            this.t20.Size = new System.Drawing.Size(753, 390);
             this.t20.TabIndex = 10;
             this.t20.Text = "tabPage8";
             this.t20.UseVisualStyleBackColor = true;
@@ -240,7 +227,7 @@
             this.t21.Location = new System.Drawing.Point(4, 22);
             this.t21.Name = "t21";
             this.t21.Padding = new System.Windows.Forms.Padding(3);
-            this.t21.Size = new System.Drawing.Size(750, 390);
+            this.t21.Size = new System.Drawing.Size(753, 390);
             this.t21.TabIndex = 11;
             this.t21.Text = "tabPage9";
             this.t21.UseVisualStyleBackColor = true;
@@ -250,7 +237,7 @@
             this.t5.Location = new System.Drawing.Point(4, 22);
             this.t5.Name = "t5";
             this.t5.Padding = new System.Windows.Forms.Padding(3);
-            this.t5.Size = new System.Drawing.Size(750, 390);
+            this.t5.Size = new System.Drawing.Size(753, 390);
             this.t5.TabIndex = 12;
             this.t5.Text = "tabPage10";
             this.t5.UseVisualStyleBackColor = true;
@@ -260,7 +247,7 @@
             this.t6.Location = new System.Drawing.Point(4, 22);
             this.t6.Name = "t6";
             this.t6.Padding = new System.Windows.Forms.Padding(3);
-            this.t6.Size = new System.Drawing.Size(750, 390);
+            this.t6.Size = new System.Drawing.Size(753, 390);
             this.t6.TabIndex = 13;
             this.t6.Text = "tabPage11";
             this.t6.UseVisualStyleBackColor = true;
@@ -270,7 +257,7 @@
             this.t7.Location = new System.Drawing.Point(4, 22);
             this.t7.Name = "t7";
             this.t7.Padding = new System.Windows.Forms.Padding(3);
-            this.t7.Size = new System.Drawing.Size(750, 390);
+            this.t7.Size = new System.Drawing.Size(753, 390);
             this.t7.TabIndex = 14;
             this.t7.Text = "tabPage12";
             this.t7.UseVisualStyleBackColor = true;
@@ -280,7 +267,7 @@
             this.t8.Location = new System.Drawing.Point(4, 22);
             this.t8.Name = "t8";
             this.t8.Padding = new System.Windows.Forms.Padding(3);
-            this.t8.Size = new System.Drawing.Size(750, 390);
+            this.t8.Size = new System.Drawing.Size(753, 390);
             this.t8.TabIndex = 15;
             this.t8.Text = "tabPage13";
             this.t8.UseVisualStyleBackColor = true;
@@ -290,7 +277,7 @@
             this.t9.Location = new System.Drawing.Point(4, 22);
             this.t9.Name = "t9";
             this.t9.Padding = new System.Windows.Forms.Padding(3);
-            this.t9.Size = new System.Drawing.Size(750, 390);
+            this.t9.Size = new System.Drawing.Size(753, 390);
             this.t9.TabIndex = 16;
             this.t9.Text = "tabPage14";
             this.t9.UseVisualStyleBackColor = true;
@@ -300,7 +287,7 @@
             this.t10.Location = new System.Drawing.Point(4, 22);
             this.t10.Name = "t10";
             this.t10.Padding = new System.Windows.Forms.Padding(3);
-            this.t10.Size = new System.Drawing.Size(750, 390);
+            this.t10.Size = new System.Drawing.Size(753, 390);
             this.t10.TabIndex = 17;
             this.t10.Text = "tabPage15";
             this.t10.UseVisualStyleBackColor = true;
@@ -310,7 +297,7 @@
             this.t11.Location = new System.Drawing.Point(4, 22);
             this.t11.Name = "t11";
             this.t11.Padding = new System.Windows.Forms.Padding(3);
-            this.t11.Size = new System.Drawing.Size(750, 390);
+            this.t11.Size = new System.Drawing.Size(753, 390);
             this.t11.TabIndex = 18;
             this.t11.Text = "tabPage16";
             this.t11.UseVisualStyleBackColor = true;
@@ -320,7 +307,7 @@
             this.t12.Location = new System.Drawing.Point(4, 22);
             this.t12.Name = "t12";
             this.t12.Padding = new System.Windows.Forms.Padding(3);
-            this.t12.Size = new System.Drawing.Size(750, 390);
+            this.t12.Size = new System.Drawing.Size(753, 390);
             this.t12.TabIndex = 19;
             this.t12.Text = "tabPage17";
             this.t12.UseVisualStyleBackColor = true;
@@ -330,10 +317,63 @@
             this.t13.Location = new System.Drawing.Point(4, 22);
             this.t13.Name = "t13";
             this.t13.Padding = new System.Windows.Forms.Padding(3);
-            this.t13.Size = new System.Drawing.Size(750, 390);
+            this.t13.Size = new System.Drawing.Size(753, 390);
             this.t13.TabIndex = 20;
             this.t13.Text = "tabPage18";
             this.t13.UseVisualStyleBackColor = true;
+            // 
+            // sol1
+            // 
+            this.sol1.Image = global::LA_Hacks_2015.Properties.Resources.SampleBubble1;
+            this.sol1.Location = new System.Drawing.Point(137, 112);
+            this.sol1.Name = "sol1";
+            this.sol1.Size = new System.Drawing.Size(82, 69);
+            this.sol1.TabIndex = 0;
+            this.sol1.TabStop = false;
+            this.sol1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // t2
+            // 
+            this.t2.Controls.Add(this.pictureBox2);
+            this.t2.Controls.Add(this.pictureBox4);
+            this.t2.Controls.Add(this.pictureBox3);
+            this.t2.Controls.Add(this.sol1);
+            this.t2.Location = new System.Drawing.Point(4, 22);
+            this.t2.Name = "t2";
+            this.t2.Padding = new System.Windows.Forms.Padding(3);
+            this.t2.Size = new System.Drawing.Size(750, 390);
+            this.t2.TabIndex = 1;
+            this.t2.Text = "t2";
+            this.t2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Black;
+            this.pictureBox3.Location = new System.Drawing.Point(369, 112);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(82, 69);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox3_DragDrop);
+            this.pictureBox3.DragEnter += new System.Windows.Forms.DragEventHandler(this.pictureBox3_DragEnter);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Black;
+            this.pictureBox2.Location = new System.Drawing.Point(385, 215);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(82, 69);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::LA_Hacks_2015.Properties.Resources.SampleBubble1;
+            this.pictureBox4.Location = new System.Drawing.Point(153, 215);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(82, 69);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
             // 
             // Form1
             // 
@@ -345,11 +385,15 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl1.ResumeLayout(false);
             this.t1.ResumeLayout(false);
-            this.t2.ResumeLayout(false);
-            this.t2.PerformLayout();
             this.t3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.sol1)).EndInit();
+            this.t2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -359,10 +403,8 @@
         private System.Windows.Forms.Button Start;
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage t1;
-        private System.Windows.Forms.TabPage t2;
         private System.Windows.Forms.TabPage t3;
         private System.Windows.Forms.Button TEST;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TabPage t4;
         private System.Windows.Forms.TabPage t14;
         private System.Windows.Forms.TabPage t15;
@@ -381,6 +423,11 @@
         private System.Windows.Forms.TabPage t11;
         private System.Windows.Forms.TabPage t12;
         private System.Windows.Forms.TabPage t13;
+        private System.Windows.Forms.TabPage t2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox sol1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox4;
     }
 }
 
--- LA Hacks 2015/Form1.cs
+++ LA Hacks 2015/Form1.cs
@@ -18,12 +18,34 @@ namespace LA_Hacks_2015
 
         private void Start_Click(object sender, EventArgs e)
         {
-            tabControl1.SelectTab("tabPage2");
+            tabControl1.SelectTab("t2");
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            sol1.DoDragDrop(sol1.Image, DragDropEffects.Copy);
+        }
+
+        private void pictureBox3_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void pictureBox3_DragDrop(object sender, DragEventArgs e)
+        {
+            pictureBox3.Image = (Image)e.Data.GetData(DataFormats.Bitmap);
+            sol1.Hide();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox3.AllowDrop = true;
+            pictureBox2.AllowDrop = true;
         }
     }
 }
